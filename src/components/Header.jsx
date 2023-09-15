dfedfe6b863a90a167e8253911f4689cb3d26c13
Maqsadbek <110594640+MAQSADBEK77@users.noreply.github.com>
2023-09-15 13:54:27 +0500

    Update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,16 +24,19 @@ function Header() {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             <li>
-              <a>Item 1</a>
+              <a href="#">Asosiy</a>
             </li>
             <li>
-              <a>Parent</a>
+              <a>Universitet haqida</a>
               <ul className="p-2">
                 <li>
-                  <a>Submenu 1</a>
+                  <a>Halqaro bo'lim</a>
                 </li>
                 <li>
-                  <a>Submenu 2</a>
+                  <a>Madaniyat bo'limi</a>
+                </li>
+                <li>
+                  <a>Ilmiy bo'limi</a>
                 </li>
               </ul>
             </li>
@@ -47,17 +50,22 @@ function Header() {
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
+            <a>Asosiy</a>
           </li>
           <li tabIndex={0}>
             <details>
-              <summary>Parent</summary>
+              <summary>
+                <a>Universitet haqida</a>
+              </summary>
               <ul className="p-2">
                 <li>
-                  <a>Submenu 1</a>
+                  <a>Halqaro bo'lim</a>
                 </li>
                 <li>
-                  <a>Submenu 2</a>
+                  <a>Madaniyat bo'limi</a>
+                </li>
+                <li>
+                  <a>Ilmiy bo'limi</a>
                 </li>
               </ul>
             </details>
